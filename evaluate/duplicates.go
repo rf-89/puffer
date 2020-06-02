85c579dcc715dbf6c982adbd5b33ae1e6e19ab21
terumasa,watanabe <message.t.watanabe@gmail.com>
2020-06-03 06:23:22 +0900

    fixed to remove duplicates.

--- evaluate/duplicates.go
+++ evaluate/duplicates.go
@@ -7,6 +7,10 @@ func Duplicates(fileInfo []map[string]string) []map[string]string {
 	for _, ref := range fileInfo {
 		for _, v := range fileInfo {
 			if ref[fileKey] != v[fileKey] && ref[hashKey] == v[hashKey] {
+				if mapArrayContains(duplicates, fileKey, v[fileKey]) ||
+					mapArrayContains(duplicates, hashKey, v[hashKey]) {
+					continue
+				}
 				m := map[string]string{}
 				m[fileKey] = v[fileKey]
 				m[hashKey] = v[hashKey]
@@ -18,4 +22,14 @@ func Duplicates(fileInfo []map[string]string) []map[string]string {
 		}
 	}
 	return duplicates
+}
+
+//配列の中に特定の文字列が含まれるかを返す
+func mapArrayContains(maps []map[string]string, key, str string) bool {
+	for _, v := range maps {
+		if v[key] == str {
+			return true
+		}
+	}
+	return false
 }
